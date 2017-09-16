294828bff13d1b1b8d9aca7480d2aaefd83fae22
rdhaundiyal <rdhaundiyal@hotmail.com>
2017-09-16 10:55:59 +0530

    Updated from the local repository

--- Feature/Gallery/Piccolo.Feature.Gallery/Controllers/GalleryController.cs
+++ Feature/Gallery/Piccolo.Feature.Gallery/Controllers/GalleryController.cs
@@ -1,18 +1,14 @@
-﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using Glass.Mapper.Sc;
 using Piccolo.Feature.Gallery.Models;
-using Piccolo.Feature.Gallery.Repository;
+using Piccolo.Foundation.Base.Controllers;
 using Piccolo.Foundation.Repository.Interface;
 
 namespace Piccolo.Feature.Gallery.Controllers
 {
-    public class GalleryController : Controller
+    public class GalleryController : BaseController
     {
-        private IRepository<ImageArticle> _imageArticleRepository;
+        private readonly IRepository<ImageArticle> _imageArticleRepository;
         public GalleryController(IRepository<ImageArticle> imageArticleRepository)
         {
             _imageArticleRepository = imageArticleRepository;
@@ -21,28 +17,20 @@ namespace Piccolo.Feature.Gallery.Controllers
         public ActionResult List()
         {
             var item = Sitecore.Mvc.Presentation.RenderingContext.Current.Rendering.Item;
-            var context = new SitecoreContext();
-            var result = context.GetItem<GalleryListViewModel>(item.Paths.Path);
+            var result = GetItem<GalleryListViewModel>(item.Paths.Path);
             return View("List", result);
         }
 
         public ActionResult Detail()
         {
-            var context = new SitecoreContext();
-            var result = context.GetCurrentItem<ImageArticle>();
-
-           
+            var result = GetItem<ImageArticle>();
             return View(result);
         }
 
         public ActionResult Search(string searchTerm = "")
         {
             var rootPath = Sitecore.Context.Database.GetItem("/sitecore/content/Home/Album");
-
-          //  var repository=new ImageArticleRepository1();
-            var dict = new Dictionary<string, object>();
-            dict.Add("Services","Web");
-            //var result = repository.GetArticles(rootPath.ID,searchTerm);
+            var dict = new Dictionary<string, object> { { "Services", "Web" } };
             var result = _imageArticleRepository.Get(rootPath.ID, dict);
             return View(result);
         }
