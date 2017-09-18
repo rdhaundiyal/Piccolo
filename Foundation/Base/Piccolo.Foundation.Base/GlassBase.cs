89168d75ec517dbea76dce95f792262eb98c95e4
rdhaundiyal <rdhaundiyal@hotmail.com>
2017-09-18 20:14:34 +0530

    Update GlassBase.cs

--- Foundation/Base/Piccolo.Foundation.Base/GlassBase.cs
+++ Foundation/Base/Piccolo.Foundation.Base/GlassBase.cs
@@ -1,11 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Glass.Mapper.Sc.Configuration;
+using Glass.Mapper.Sc.Configuration.Attributes;
 
 namespace Piccolo.Foundation.Base
 {
+    [SitecoreType(AutoMap = true)]
     public class GlassBase
     {
+        [SitecoreId]
+        public virtual Guid Id { get; set; }
+
+        [SitecoreInfo(SitecoreInfoType.Url)]
+        public virtual string Url { get; set; }
+
+        [SitecoreInfo(SitecoreInfoType.Name)]
+        public virtual string Name { get; set; }
+        [SitecoreInfo(SitecoreInfoType.TemplateName)]
+        public virtual string Template { get; set; }
     }
 }
