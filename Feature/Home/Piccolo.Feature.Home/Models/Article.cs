82a461eab4607cdd73e431a4f929e1a6e6cda94b
rdhaundiyal <rdhaundiyal@hotmail.com>
2017-09-18 19:59:00 +0530

    Update Article.cs

--- Feature/Home/Piccolo.Feature.Home/Models/Article.cs
+++ Feature/Home/Piccolo.Feature.Home/Models/Article.cs
@@ -1,11 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Text;
+using Glass.Mapper.Sc.Configuration;
+using Glass.Mapper.Sc.Configuration.Attributes;
+using Glass.Mapper.Sc.Fields;
+using Piccolo.Foundation.Base;
 
 namespace Piccolo.Feature.Home.Models
 {
-    public class Article
+  public   class Article:GlassBase
     {
+        [SitecoreField]
+        string Title { get; set; }
+        [SitecoreField]
+        string SubTitle { get; set; }
+        [SitecoreField]
+        string Description { get; set; }
+        [SitecoreField(FieldType = SitecoreFieldType.GeneralLink, FieldName = "Call To Action")]
+        Link CallToAction { get; set; }
     }
 }
