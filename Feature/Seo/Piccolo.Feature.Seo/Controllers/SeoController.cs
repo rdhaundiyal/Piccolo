bfd0f4cdf9e9fcb7218053f34071b6d527a64c67
rdhaundiyal <rdhaundiyal@hotmail.com>
2017-09-18 20:11:15 +0530

    Update SeoController.cs

--- Feature/Seo/Piccolo.Feature.Seo/Controllers/SeoController.cs
+++ Feature/Seo/Piccolo.Feature.Seo/Controllers/SeoController.cs
@@ -1,17 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Glass.Mapper.Sc;
+using Piccolo.Feature.Seo.Models;
+using Piccolo.Foundation.Base.Controllers;
 
 namespace Piccolo.Feature.Seo.Controllers
 {
-    public class SeoController : Controller
+    public class SeoController : BaseController
     {
-        // GET: Seo
-        public ActionResult Index()
+        //
+        // GET: /Seo/
+        public ActionResult PageMetadata()
         {
-            return View();
+            
+            var result = GetItem<Metadata>();
+            return View(result);
         }
-    }
+	}
 }
