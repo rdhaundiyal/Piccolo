da7f0fd0ce42bd405b84a090532c30c9575d2b41
rdhaundiyal <rdhaundiyal@hotmail.com>
2017-09-18 20:12:12 +0530

    Update Metadata.cs

--- Feature/Seo/Piccolo.Feature.Seo/Models/Metadata.cs
+++ Feature/Seo/Piccolo.Feature.Seo/Models/Metadata.cs
@@ -1,11 +1,26 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
-using System.Web;
+using System.Text;
+using System.Threading.Tasks;
+using Glass.Mapper.Sc.Configuration;
+using Glass.Mapper.Sc.Configuration.Attributes;
+using Piccolo.Foundation.Base;
 
 namespace Piccolo.Feature.Seo.Models
 {
-    public class Metadata
+  public  class Metadata:GlassBase
     {
+    public    string PageTitle { get; set; }
+    public string SiteTitle { get; set; }
+            [SitecoreField( FieldName = "Metadata-Description")]
+    public string Description { get; set; }
+        public    string KeywordsList { get; set; }
+            [SitecoreField( FieldName = "Metadata-Title")]
+            public string Title { get; set; }
+            public ICollection<string> Robots { get; set; }
+     
+            public NameValueCollection CustomMetadata { get; set; }
     }
 }
