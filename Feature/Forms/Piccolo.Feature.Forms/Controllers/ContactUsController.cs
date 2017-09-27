4d55b2c913d00cfca6a6a4d4d008d36a007e36ea
rdhaundiyal <rdhaundiyal@hotmail.com>
2017-09-27 14:05:11 +0530

    Added new project for forms

---
+++ Feature/Forms/Piccolo.Feature.Forms/Controllers/ContactUsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Piccolo.Feature.Forms.Controllers
+{
+    public class ContactUsController : Controller
+    {
+        //
+        // GET: /ContactUs/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
