0e56802c57d5227f7480a0d0d6839c0c826979f2
rdhaundiyal <rdhaundiyal@hotmail.com>
2017-09-27 14:16:25 +0530

    Update Contact.cs

--- Feature/Forms/Piccolo.Feature.Forms/Models/Contact.cs
+++ Feature/Forms/Piccolo.Feature.Forms/Models/Contact.cs
@@ -1,11 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-
-namespace Piccolo.Feature.Forms.Views
+using System.Text;
+using Glass.Mapper.Sc.Configuration;
+using Glass.Mapper.Sc.Configuration.Attributes;
+using Glass.Mapper.Sc.Fields;
+using Piccolo.Foundation.Base;
+namespace Piccolo.Feature.Forms.Models
 {
-    public class Contact
+    public class Contact:GlassBase
     {
+        [SitecoreField]
+        public string Title { get; set; }
+        [SitecoreField(FieldName = "SubTitle")]
+        public string Description { get; set; }
+        public string ContactName { get; set; }
+        public string Email { get; set; }
+        public string Website { get; set; }
+        public string Body { get; set; }
+
+        public string Message { get; set; }
     }
 }
